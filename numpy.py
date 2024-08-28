5e9dedb28ec990a19f8aea4cbd50a2e2aea67981
ivanrodriguez01 <119910201+ivanrodriguez01@users.noreply.github.com>
2024-08-28 03:58:34 -0400

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,1 +1,2 @@
-#Numpy is the fundamental package for numberic computing with Python. It provides powereful ways to create, store, and/or manipulate data.
+'''Numpy is the fundamental package for numberic computing with Python. 
+It provides powereful ways to create, store, and/or manipulate data.'''
